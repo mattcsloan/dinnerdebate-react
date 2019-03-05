46385b7ac0249f2cbb02dfafc0585f6de39d3afa
Matt Sloan <sloan.782@gmail.com>
2019-03-05 16:17:42 -0500

    Added Auth0 authentication and login flow to client

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -3,7 +3,7 @@ import { Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Recipes from './components/Recipes';
 import Recipe from './components/Recipe';
-
+import Callback from './components/Callback';
 import './assets/styles/app.scss';
 
 class App extends Component {
@@ -14,6 +14,7 @@ class App extends Component {
         <div className="wrapper">
           <Route exact path='/' component={Recipes} />
           <Route exact path='/recipes/:recipeId' component={Recipe} />
+          <Route exact path='/callback' component={Callback} />
         </div>
       </div>
     );
--- src/client/src/components/Navigation/Navigation.js
+++ src/client/src/components/Navigation/Navigation.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import auth0Client from '../Authentication';
 
-function Navigation() {
+function Navigation(props) {
+  const signOut = () => {
+    auth0Client.signOut();
+    props.history.replace('/');
+  };
+
   return (
     <nav className="navigation">
       <div className="wrapper">
         <Link to="/">
           Recipes
         </Link>
+        {!auth0Client.isAuthenticated() &&
+          <a onClick={auth0Client.signIn}>Sign In</a>
+        }
+        {auth0Client.isAuthenticated() &&
+          <div>
+            <label>{auth0Client.getProfile().name}</label>
+            <a onClick={() => {signOut()}}>Sign Out</a>
+          </div>
+        }
       </div>
     </nav>
   );
 }
 
-export default Navigation;
+export default withRouter(Navigation);
